030cf6eb8806fcda26410c9474d960857faae803
Ayush Sharma <arp23359@gmail.com>
2024-02-04 03:04:32 +0530

    Eventlet For Prod.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+import eventlet
+
+eventlet.monkey_patch()
 
 from flask import render_template, redirect, url_for, session, Blueprint,request,jsonify
 from flask_login import login_required, current_user
